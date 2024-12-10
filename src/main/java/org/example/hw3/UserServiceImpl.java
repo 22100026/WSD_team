8f2bf1ebe4b99b5e3359b771b16911e9381c13ef
DowookKim <156278491+DowookKim@users.noreply.github.com>
2024-12-10 10:59:06 +0900

    easy

---
+++ src/main/java/org/example/hw3/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package org.example.hw3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+    @Autowired
+    UserDAO userDAO;
+    public UserVO getUser(UserVO vo){
+        return userDAO.getUser(vo);
+    }
+}
---
+++ src/main/java/org/example/hw3/UserVO.java
@@ -0,0 +1,28 @@
+package org.example.hw3;
+
+public class UserVO {
+    private String userid;
+
+    private String password;
+
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+}
+
